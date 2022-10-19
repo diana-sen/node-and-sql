dbffd6f54f2e680997e33c5c20985b112e9c188b
Diana Sen <dianasen13@gmail.com>
2022-10-18 20:58:25 -0500

    chore: url params and queries

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,32 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const package = require('../package.json')
+const time= Date.now();
+const now= new Date(time);
+
+console.log(package)
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send( { title: 'Express with SQL' });
+router.get('/', (req, res, next) =>  res.send( { name: package.name, date: now.toString() }));
+
+// URL with parameters
+router.get('/users/:name', (req, res, next) =>  {
+  console.log(req.params);
+  res.send( { name: req.params.name });
 });
+
+router.get('/users/:name/:id', (req, res, next) =>  {
+  console.log(req.params)
+  res.send( { name: req.params.name , id: req.params.id });
+});
+
+// URL with query string
+router.get('/list', (req, res, next) => {
+  let numbers =[ 51, 92, 31, 45, 50, 96];
+
+  console.log(req.query)
+  res.send( { number: req.query.sort ? numbers.sort() : numbers });
+}); 
 
 /* GET health page. */
 /* router.get('/health', function(req, res, next) {
